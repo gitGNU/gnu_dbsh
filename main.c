56c4d533c2b2d584c00d78c38d5da605172d8101
Ben Spencer <dangerous.ben@gmail.com>
2007-12-23 17:25:30 +0000

    Moved TODO list into its own file and added copyright message.

--- main.c
+++ main.c
@@ -1,27 +1,20 @@
 /*
- * TODO: trap Ctrl-C and kill query instead (SQLCancel?), or just clear buffer if we're not running a query
- *       multiple result sets (SQLMoreResults)
- *       batch mode?
- *       tab completion - commands as a minimum, maybe tables etc too?
- *       regression tests using SQLite
- *       cope with result set being too large to fit in memory
- *       separate history by DSN?
- *       newlines in data break horizontal display
- *       reconnect if connection dies
- *       do away with line numbers, since they don't make sense anyway? (or make them make sense by counting newlines?)
- *       specify editor as argument to \e? (or named buffer?)
- *       text truncated?
- *       actions as shortcuts for commands, eg \r for reconnect (or do away with commands and only have actions?)
- *
- *       prepared statement support:
- *       SELECT * FROM <table> WHERE id = ?
- *       \g 1
- *       \g 2
- *       etc
- *
- *       commands:
- *       *autocommit on|off
- */
+    dbsh - text-based ODBC client
+    Copyright (C) 2007 Ben Spencer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <locale.h>
 #include <signal.h>
