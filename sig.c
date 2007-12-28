398af7e1818e8a4a3e85d90abd1b9523a02ac2a1
Ben Spencer <dangerous.ben@gmail.com>
2007-12-28 00:14:12 +0000

    Ignore SIGPIPE.

--- sig.c
+++ sig.c
@@ -32,12 +32,14 @@ void signal_handler_install()
 {
 	struct sigaction act;
 
-	act.sa_handler = signal_handler;
-	act.sa_flags   = 0;
-
+	act.sa_flags = 0;
 	sigemptyset(&act.sa_mask);
 
-	if(sigaction(SIGINT, &act, (struct sigaction *) 0) == -1) {
+	act.sa_handler = signal_handler;
+	if(sigaction(SIGINT, &act, (struct sigaction *) 0) == -1)
 		perror("Failed to set signal handler");
-	}
+
+	act.sa_handler = SIG_IGN;
+	if(sigaction(SIGPIPE, &act, (struct sigaction *) 0) == -1)
+		perror("Failed to set signal handler");
 }
