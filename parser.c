089c7b76442f743f3b2a3ae3cffb223e2b155741
Ben Spencer <dangerous.ben@gmail.com>
2008-01-02 19:06:58 +0000

    Don't recognise pipe characters within quotes.

--- parser.c
+++ parser.c
@@ -92,7 +92,7 @@ static void parse_char(char c, parser_state *st)
 				return;
 			}
 
-			if(c == '>' || c == '|') st->pipe = 1;
+			if((c == '>' || c == '|') && !st->quote) st->pipe = 1;
 		}
 	}
 
